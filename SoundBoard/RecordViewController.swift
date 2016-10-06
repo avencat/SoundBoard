3cad9aa57a2704622a660759475018e269735c23
Axel Vencatareddy <avencatareddy@me.com>
2016-10-06 14:26:55 +0200

    Microphone is recording and tableView displays the song, the swipe to delete is implemented.

--- SoundBoard/RecordViewController.swift
+++ SoundBoard/RecordViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 import AVFoundation
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, UITextFieldDelegate {
   
   @IBOutlet weak var titleTextField: UITextField!
   @IBOutlet weak var playButton: UIButton!
   @IBOutlet weak var addButton: UIButton!
+  @IBOutlet weak var recordButton: UIButton!
   
   var audioRecorder: AVAudioRecorder?
+  var audioPlayer: AVAudioPlayer?
+  var audioURL : URL?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     setupRecorder()
+    playButton.isEnabled = false
+    addButton.isEnabled = false
+    titleTextField.delegate = self
   }
   
   func setupRecorder() {
@@ -36,32 +42,74 @@ class RecordViewController: UIViewController {
       // Create URL for the Audio Recorder
       let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
       let pathComponents = [basePath, "audio.m4a"]
-      let audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+      audioURL = NSURL.fileURL(withPathComponents: pathComponents)
       
       // Create Settings for the Audio Recorder
       var settings: [String : AnyObject] = [:]
-      settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
-      settings[AVSampleRateKey] = 44100.0 as AnyObject?
-      settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+      settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject
+      settings[AVSampleRateKey] = 44100.0 as AnyObject
+      settings[AVNumberOfChannelsKey] = 2 as AnyObject
       
       
       // Create AudioRecorder Object
       
       audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
       audioRecorder?.prepareToRecord()
-    } catch {
-      print("An error occured while setting up the session")
+    } catch let error as NSError {
+      print("An error occured while setting up the session : \(error)")
     }
     
 
   }
   
   @IBAction func recordTapped(_ sender: AnyObject) {
+
+    if audioRecorder!.isRecording {
+      // Stop the recording
+      audioRecorder!.stop()
+      
+      // Change button title to Record
+      recordButton.setTitle("Record", for: .normal)
+      
+      
+      playButton.isEnabled = true
+      addButton.isEnabled = true
+    } else {
+      // Start the recording
+      audioRecorder!.record()
+      
+      // Change button title to Stop
+      recordButton.setTitle("Stop", for: .normal)
+    }
   }
   
   @IBAction func playTapped(_ sender: AnyObject) {
+    do {
+      try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+      audioPlayer!.play()
+    } catch {}
   }
   
   @IBAction func addTapped(_ sender: AnyObject) {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let audio = Audio(context: context)
+    
+    if titleTextField.text != nil {
+      audio.title = titleTextField.text
+    } else {
+      audio.title = "audio without name"
+    }
+    audio.audio = NSData(contentsOf: audioURL!)
+    
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    
+    // Pop the ViewController
+    navigationController!.popViewController(animated: true)
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    self.view.endEditing(true)
+    return false
   }
 }
--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  @IBOutlet weak var tableView: UITableView!
+  
+  var audios: [Audio] = []
+  var audioPlayer: AVAudioPlayer?
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    
+    // Fetch the audios and reload data
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    do {
+      try audios = context.fetch(Audio.fetchRequest())
+      tableView.reloadData()
+    } catch {}
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return audios.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    
+    cell.textLabel?.text = audios[indexPath.row].title
+    
+    return cell
   }
 
-
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let audio = audios[indexPath.row]
+    do {
+      try audioPlayer = AVAudioPlayer(data: audio.audio as! Data)
+      audioPlayer?.play()
+    } catch {}
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+      context.delete(audios[indexPath.row])
+      (UIApplication.shared.delegate as! AppDelegate).saveContext()
+      do {
+        try audios = context.fetch(Audio.fetchRequest())
+        tableView.reloadData()
+      } catch {}
+    }
+  }
 }
 
